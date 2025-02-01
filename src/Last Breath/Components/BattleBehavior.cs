eb9942702349eb5876ae29ffd63e16e738d2e0be
Toddless <faustonlyone@gmail.com>
2025-02-01 18:39:43 +0100

    Create new branch

--- src/Last Breath/Components/BattleBehavior.cs
+++ src/Last Breath/Components/BattleBehavior.cs
@@ -1,14 +1,11 @@
 ﻿namespace Playground.Components
 {
-    using Godot;
     using Playground.Script;
     using Playground.Script.Passives.Attacks;
 
     // TODO: Need to rework completely
-    [Inject]
     public class BattleBehavior
     {
-        private RandomNumberGenerator? _rnd;
         private BaseEnemy? _enemyBase;
         private Player? _player;
         private ICharacter? _selfTarget;
@@ -19,13 +16,6 @@
             _enemyBase = enemy;
             _selfTarget = enemy;
             DiContainer.InjectDependencies(this);
-        }
-
-        [Inject]
-        protected RandomNumberGenerator? Rnd
-        {
-            get => _rnd;
-            set => _rnd = value;
         }
 
         public void GatherInfo(Player player)
