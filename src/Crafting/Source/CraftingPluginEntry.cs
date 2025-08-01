06b8dd793dbc9a652f76195d5cafadf73440af2e
Rafael Proskurin <rafaelproskurin@gmail.com>
2025-08-01 13:09:08 +0200

    Created crafting system as new project. Created new Contracts project (If the projects require the same interfaces)

---
+++ src/Crafting/Source/CraftingPluginEntry.cs
@@ -0,0 +1,13 @@
+﻿namespace Crafting
+{
+    using Godot;
+
+    [Tool]
+    public partial class CraftingPluginEntry : EditorPlugin
+    {
+        public override void _EnterTree()
+        {
+            GD.Print("Crafting plugin entered");
+        }
+    }
+}
