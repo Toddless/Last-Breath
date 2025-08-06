44bda28453a0feb028cc531709443cce8937a8a6
Rafael Proskurin <rafaelproskurin@gmail.com>
2025-08-06 14:14:36 +0200

    WIP

---
+++ src/Core/Interfaces/Crafting/IMaterialModifiers.cs
@@ -0,0 +1,11 @@
+﻿namespace Core.Interfaces.Crafting
+{
+    using Core.Enums;
+
+    public interface IMaterialModifiers
+    {
+        ModifierType ModifierType { get; set; }
+        Parameter Parameter { get; set; }
+        float Value { get; set; }
+    }
+}
